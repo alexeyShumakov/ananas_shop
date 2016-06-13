c9690fe5874cb4a866dd1be139be9f085b3d9469
alexey Shumakov <bobshaftoe@yandex.ru>
2016-06-13 20:43:06 +0500

    add ancestry to cateogries

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,2 +1,3 @@
 class Category < ActiveRecord::Base
+  has_ancestry
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160613091924) do
+ActiveRecord::Schema.define(version: 20160613144937) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -20,6 +20,7 @@ ActiveRecord::Schema.define(version: 20160613091924) do
     t.string   "title"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string   "ancestry"
   end
 
 end

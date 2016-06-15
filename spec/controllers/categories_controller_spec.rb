7b0a22300284aa79ec30a1588d6a25c1ddc2bac7
alexey Shumakov <bobshaftoe@yandex.ru>
2016-06-15 10:22:32 +0500

    test controllers

--- spec/controllers/categories_controller_spec.rb
+++ spec/controllers/categories_controller_spec.rb
@@ -10,10 +10,14 @@ RSpec.describe CategoriesController, type: :controller do
     end
 
     it "assigns @category" do
-      category = create :category
       get :show, { id: category.id }
-
       expect(assigns[:category]).to eq(category)
+    end
+
+    it 'assigns @products' do
+      product = create :product, category: category
+      get :show, { id: category.id }
+      expect(assigns(:products)).to eq([product])
     end
   end
 
--- spec/controllers/products_controller_spec.rb
+++ spec/controllers/products_controller_spec.rb
@@ -1,5 +1,19 @@
 require 'rails_helper'
 
-
 RSpec.describe ProductsController, type: :controller do
+  describe 'GET #show' do
+    it 'assigns @product' do
+      product = create(:product)
+      get :show, { id: product.id }
+      expect(assigns(:product)).to eq(product)
+    end
+  end
+
+  describe 'GET #index' do
+    it 'assigns @products' do
+      product = create(:product)
+      get :index
+      expect(assigns(:products)).to eq([product])
+    end
+  end
 end

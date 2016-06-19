17919809593c2a45f06e3a582d5ab76675b70530
alexey Shumakov <bobshaftoe@yandex.ru>
2016-06-19 12:33:53 +0500

    fix categories seeds

--- db/fixtures/categories.rb
+++ db/fixtures/categories.rb
@@ -1,16 +1,14 @@
-root_categories = [
+Category.seed(:id,
   { id: 1, title: Faker::Commerce.department(2, true) },
   { id: 2, title: Faker::Commerce.department(2, true) },
   { id: 3, title: Faker::Commerce.department(2, true) },
   { id: 4, title: Faker::Commerce.department(2, true) }
-]
+)
 
-descendants_categories = [
-  { id: 5, title: Faker::Commerce.department(2, true), parent: Category.find(1) },
+Category.seed(:id, { id: 5, title: Faker::Commerce.department(2, true), parent: Category.find(1) })
+Category.seed(:id,
   { id: 6, title: Faker::Commerce.department(2, true), parent: Category.find(1) },
   { id: 7, title: Faker::Commerce.department(2, true), parent: Category.find(1) },
   { id: 8, title: Faker::Commerce.department(2, true), parent: Category.find(5) },
-  { id: 9, title: Faker::Commerce.department(2, true), parent: Category.find(5) },
-]
-Category.seed(:id, root_categories)
-Category.seed(:id, descendants_categories)
+  { id: 9, title: Faker::Commerce.department(2, true), parent: Category.find(5) }
+)

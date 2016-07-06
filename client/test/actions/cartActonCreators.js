8c0ab8fe5ae74d4a5e4f11a4320ca8634861b983
alexey Shumakov <bobshaftoe@yandex.ru>
2016-07-06 16:56:32 +0500

    test async cart actions

--- client/test/actions/cartActonCreators.js
+++ client/test/actions/cartActonCreators.js
@@ -1,8 +1,92 @@
-import expect from 'expect'
+import expect from 'expect';
+import configureMockStore from 'redux-mock-store';
+import thunk from 'redux-thunk';
+import axios from 'axios';
+import sinon from 'sinon';
 
 import * as actions from '../../app/bundles/AnanasShop/actions/cartActionCreators';
 import actionTypes from '../../app/bundles/AnanasShop/constants/cartConstants';
+
+const middlewares = [ thunk ];
+const mockStore = configureMockStore(middlewares);
+
 describe('cart actions', () => {
+  describe('async', () => {
+    let id = 1;
+    let server;
+    beforeEach(() => {
+      server = sinon.fakeServer.create();
+      server.autoRespond = true;
+    });
+
+    afterEach(() => {
+      server.restore();
+    })
+
+    it('#fetchCart', () => {
+      server.respondWith(
+        [200, { "Content-Type": "application/json" }, JSON.stringify({cart: 'cart'})]
+      );
+
+      const expectedActions = [
+        { type: actionTypes.SET_CART_LOADING_STATE, loadingState: true },
+        { type: actionTypes.SET_CART, cart: 'cart' },
+        { type: actionTypes.SET_CART_LOADING_STATE, loadingState: false},
+      ]
+      const store = mockStore({ cart: '' });
+
+      return store.dispatch(actions.fetchCart(id))
+        .then(() => {
+          expect(store.getActions()).toEqual(expectedActions);
+        });
+      });
+
+    it('#addToCart', () => {
+      server.respondWith(
+        [200, { "Content-Type": "application/json" }, JSON.stringify({cart: 'cart'})]
+      );
+
+      const expectedActions = [
+        { type: actionTypes.RESET_SELECTED },
+        { type: actionTypes.SET_SELECTED, productId: id},
+        { type: actionTypes.SET_CART, cart: 'cart' }
+      ];
+      const store = mockStore({ cart: '' });
+
+      return store.dispatch(actions.addToCart(id))
+        .then(() => {
+          expect(store.getActions()).toEqual(expectedActions);
+        });
+    });
+
+    it('#updateLineItem', () => {
+      server.respondWith(
+        [200, { "Content-Type": "application/json" }, JSON.stringify({cart: 'cart'})]
+      );
+
+      const expectedActions = [ { type: actionTypes.SET_CART, cart: 'cart' } ];
+      const store = mockStore({ cart: '' });
+      return store.dispatch(actions.updateLineItem(id, 2))
+        .then(() => {
+          expect(store.getActions()).toEqual(expectedActions);
+        });
+    });
+
+    it('#destroyLineItem', () => {
+      server.respondWith(
+        [200, { "Content-Type": "application/json" }, JSON.stringify({cart: 'cart'})]
+      );
+
+      const expectedActions = [ { type: actionTypes.SET_CART, cart: 'cart' } ];
+      const store = mockStore({ cart: '' });
+      return store.dispatch(actions.destroyLineItem(id))
+        .then(() => {
+          expect(store.getActions()).toEqual(expectedActions);
+        });
+    });
+
+  });
+
   describe('sync', () => {
     it('should return set selected action', () => {
       const id = 42;

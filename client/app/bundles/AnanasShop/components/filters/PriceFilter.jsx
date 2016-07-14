8536ef8bc63b08c2ad9d81b26834721137719fb2
alexey Shumakov <bobshaftoe@yandex.ru>
2016-07-14 13:39:00 +0500

    price filter refactoring

--- client/app/bundles/AnanasShop/components/filters/PriceFilter.jsx
+++ client/app/bundles/AnanasShop/components/filters/PriceFilter.jsx
@@ -11,17 +11,14 @@ export default class PriceFilter extends React.Component {
 
   constructor(props, context) {
     super(props, context);
-    _.bindAll(this, 'blurMinInput', 'blurMaxInput', 'setFilter', 'updateFilter',
-              'fetchData', 'updateMinForm', 'updateMaxForm');
+    _.bindAll(this, 'blurInput', 'setFilter', 'updateFilter', 'fetchData', 'updateMinForm', 'updateMaxForm');
   }
 
   setFilter(value) {
     let { filter, setFilter } = this.props;
     let params = Immutable.List(value);
-    let newFilter = filter
-    .set('minB', value[0]).set('maxB', value[1])
-    .set('minFormB', value[0]).set('maxFormB', value[1])
-    .set('params', params);
+    let newFilter = filter.set('minB', value[0]).set('maxB', value[1])
+    .set('minFormB', value[0]).set('maxFormB', value[1]).set('params', params);
     setFilter(newFilter);
   }
 
@@ -31,58 +28,45 @@ export default class PriceFilter extends React.Component {
 
   updateFilter() {
     let { filter, setFilter } = this.props;
-    let minForm = filter.get('minFormB') || filter.get('min');
-    minForm = minForm.toString().match(/\d+/);
-    minForm = parseInt(minForm);
-    if (minForm < filter.get('min') || minForm > filter.get('maxB') ) {
-      minForm = filter.get('min');
-    }
-    let maxForm = filter.get('maxFormB') || filter.get('max');
-    maxForm = maxForm.toString().match(/\d+/);
-    maxForm = parseInt(maxForm);
-    if (maxForm > filter.get('max') || maxForm < filter.get('minB')) {
-      maxForm = filter.get('max');
-    }
+    let { min, max, minB, maxB, minFormB, maxFormB } = filter.toJS();
+    let minForm = this.getValidValue(minFormB, minB, min, maxB);
+    let maxForm = this.getValidValue(maxFormB, maxB, minB, max);
+
     let params = Immutable.List([minForm, maxForm]);
-    let newFilter = filter.set('minB', minForm)
-    .set('maxB', maxForm).set('params', params);
+    let newFilter = filter.set('minB', minForm).set('maxB', maxForm).set('params', params);
     this.props.updateFilter(newFilter);
   }
 
-  blurMinInput(e) {
-    if(e.key === 'Enter') {
-      ReactDOM.findDOMNode(this.refs.minInput).blur();
-    }
+  getValidValue(value, defaultValue, min, max) {
+    let temp = value || defaultValue;
+    temp = temp.toString().match(/\d+/);
+    if (_.isEmpty(temp)) { temp = defaultValue }
+    temp = parseInt(temp);
+    return _.clamp(temp, min, max)
   }
 
-  blurMaxInput(e) {
+  blurInput(e) {
     if(e.key === 'Enter') {
+      ReactDOM.findDOMNode(this.refs.minInput).blur();
       ReactDOM.findDOMNode(this.refs.maxInput).blur();
     }
   }
 
   updateMinForm(e) {
     let { filter, setFilter } = this.props;
-    let value = e.target.value;
-    let newFilter = filter.set('minFormB', value)
+    let newFilter = filter.set('minFormB', e.target.value)
     setFilter(newFilter);
   }
 
   updateMaxForm(e) {
     let { filter, setFilter } = this.props;
-    let value = e.target.value;
-    let newFilter = filter.set('maxFormB', value)
+    let newFilter = filter.set('maxFormB', e.target.value)
     setFilter(newFilter);
   }
 
   render() {
     const { filter } = this.props;
-    let min = filter.get('min');
-    let max = filter.get('max');
-    let minB = filter.get('minB');
-    let maxB = filter.get('maxB');
-    let minFormB = filter.get('minFormB');
-    let maxFormB = filter.get('maxFormB');
+    let { min, max, minB, maxB, minFormB, maxFormB } = filter.toJS();
     let value = [minB, maxB];
     return (
       <div>
@@ -92,7 +76,7 @@ export default class PriceFilter extends React.Component {
           value={minFormB}
           onChange={this.updateMinForm}
           onBlur={this.updateFilter}
-          onKeyUp={this.blurMinInput}
+          onKeyUp={this.blurInput}
           />
         <FormControl
           ref='maxInput'
@@ -100,7 +84,7 @@ export default class PriceFilter extends React.Component {
           value={maxFormB}
           onChange={this.updateMaxForm}
           onBlur={this.updateFilter}
-          onKeyUp={this.blurMaxInput}
+          onKeyUp={this.blurInput}
           />
         <Rcslider
            range
